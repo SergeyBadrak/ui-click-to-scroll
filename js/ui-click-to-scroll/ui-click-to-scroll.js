0210f44f52dfcbb4be58b2417d6e832bdc518047
SergeyBadrak <bsan@ukr.net>
2021-04-04 13:32:08 +0300

    add wordpress menu support

--- js/ui-click-to-scroll/ui-click-to-scroll.js
+++ js/ui-click-to-scroll/ui-click-to-scroll.js
@@ -1,11 +1,54 @@
 
-let el = '.click-to-scroll';
+(function($){
 
-$( el ).click(function(event) {
-    event.preventDefault();
-    var id  = $(this).attr('href'),
-        top = $(id).offset().top;
-    $('body,html').animate({scrollTop: top}, 1500, "swing");
-});
+    let el = '.click-to-scroll';
+    let id = '';
+
+    $( el ).click(function(event) {
+
+        event.preventDefault();
+
+// if clicked element has 'href' atribut
+// <li><a href="#menu1" class="click-to-scroll">Menu 1</a></li>
+        if ($(this).attr('href')) {
+            id  = $(this).attr('href');
+        }
+
+// if clicked on 'li' that has children 'a' element with 'href' atribut
+// <li class="click-to-scroll"><a href="#menu1" >Menu 1</a></li>
+        else if (($(this).children().attr('href'))) {
+            id  = $(this).children().attr('href');
+        }
+
+
+
+
+// Run Main fuctions if target element is exist on page
+        if ($(id).length) {
+            doScroll(id);
+            hideMenu();
+        }
+        else {
+            console.log('element:' + id + ' is not exist');
+        }
+
+
+
+
+// ********* Main fuctions *********
+        function doScroll(id) {
+            var top = $(id).offset().top;
+            $('body,html').animate({scrollTop: top}, 1500, "swing");
+        }
+
+// Hide content of Full Page Menu
+        function hideMenu() {
+            $('.menu-ico').removeClass('menu-ico-close');
+            $('.overlay-m').removeClass('menu_is_visible');
+        }
+
+    });
+
+})(jQuery);
 
 
